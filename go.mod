4f0639adf258d5be1a04d5d6e24e49aff19bf7cb
Zane (She/They) <neptunium0237@gmail.com>
2024-07-15 16:33:50 -0600

    basic setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/golp
+
+go 1.22.5
---
+++ golp.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf(`
+-------------------------------- Golp -------
+	`)
+
+	for {
+
+		userOptions()
+		//-- get user choice
+		var choice int 
+		fmt.Print("Your selection: ")
+		fmt.Scan( &choice ) 
+
+		//--- condition
+		if choice == 1 {
+			fmt.Printf(`
+- - - - - - - - - - - - - - - - - - - - - - - - - -
+Go variables examples: string | int | float | bool
+
+ data type inferred (integer)
+  var x = 4    x := 4
+        
+ data type stated
+  var x int = 4 
+
+- - - - - - - - - - - - - - - - - - - - - - - - - -			
+`)
+		} else if choice == 9 {
+			fmt.Println(" Exiting Golp \n")
+			break
+		} else {
+			fmt.Println(" not an option \n")
+			return
+		}
+
+
+
+	} // end of for loop
+	
+
+
+}
+
+
+func userOptions() {
+	fmt.Printf(`
+ what do you want to look up ?
+
+ 1. variables
+ 9. quit
+
+ `)
+}
+
